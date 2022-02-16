ceb3b38293be1275ff79bce51aec33da819a81fd
Polciaax <99778614+Polciaax@users.noreply.github.com>
2022-02-16 16:24:57 +0100

    kalendarz

---
+++ calculation.js
@@ -0,0 +1,26 @@
+function clearOutput(){
+	document.getElementById("output").innerHTML="0";
+}
+function removeZero(){
+	var value = document.getElementById("output").innerHTML;
+	if (value=="0") {
+		value = ""
+		document.getElementById("output").innerHTML=value;
+	}
+}
+function perc(){
+	removeZero()
+ var value = document.getElementById("output").innerHTML;
+ 	value = value/100
+    document.getElementById("output").innerHTML=value;
+}
+function fordisplay(mywalue){
+	removeZero()
+	 document.getElementById("output").innerHTML+=mywalue
+}
+function solve() {
+	removeZero()
+	var equation = document.getElementById("output").innerHTML;
+	var solved = eval(equation)
+	document.getElementById("output").innerHTML=solved;
+}
